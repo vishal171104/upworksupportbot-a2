9ecc457b2375b14345c09b5eed89fd7a778e61a0
vishal171104 <154308142+vishal171104@users.noreply.github.com>
2026-06-07 20:48:29 +0530

    Update ingest.py

--- ingest.py
+++ ingest.py
@@ -1,7 +1,7 @@
 import os
 
 import chromadb
-from langchain_text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_chroma import Chroma
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.embeddings import HuggingFaceEmbeddings
